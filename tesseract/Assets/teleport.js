8274bd2abdc21030aeaa9f47c3f5e59cfd7512ab
kitfuse <nprentiss@gmail.com>
2013-12-11 01:13:15 -0800

    murder
    two room loops. the easy straight ones.

--- tesseract/Assets/teleport.js
+++ tesseract/Assets/teleport.js
@@ -1,48 +1,11 @@
-﻿private var targetPosition : Vector3;
-private var rotated : boolean = false;
-private var worldTrans: Transform;
-private var targetpos : Vector3;
-private var camTrans : Transform;
-
-enum Direction {right, left, forward, back, up, none};
-var SpinDirection : Direction;
+﻿private var worldTrans: Transform;
 
 var target : Transform;
 
 function Awake () {
 	worldTrans = transform.root;
-	Debug.Log(worldTrans);
-	camTrans = GameObject.FindWithTag("MainCamera").transform;
-	Debug.Log(camTrans);
- }
+}
 
 function OnTriggerEnter( other : Collider ) {
-	switch ( SpinDirection ) {
-	case Direction.right:
-		Debug.Log("Direction: right");
-		worldTrans.Rotate (Vector3.right, 90);
-		break;
-	case Direction.left:
-		Debug.Log("Direction: left");
-		worldTrans.Rotate (Vector3.left, 90);
-		break;
-	case Direction.forward:
-		Debug.Log("Direction: forward");
-		worldTrans.Rotate (Vector3.forward, 90);
-		break;
-	case Direction.back:
-		Debug.Log("Direction: back");
-		worldTrans.Rotate (Vector3.back, 90);
-		break;
-	case Direction.up:
-		Debug.Log("Direction: upside down");
-		worldTrans.Rotate (Vector3.forward, 180);
-		break;
-	case Direction.none:
-		Debug.Log("Direction: none");
-		break;
-	default:
-		Debug.Log ("No direction choice");
-	}
 	other.transform.position = target.position;
  }
---
+++ tesseract/Assets/twist.js
@@ -0,0 +1,31 @@
+﻿private var worldTrans: Transform;
+
+enum Direction {right, left, back};
+var SpinDirection : Direction;
+
+var target : Transform;
+
+function Awake () {
+	worldTrans = transform.root;
+	Debug.Log(worldTrans);
+ }
+
+function OnTriggerEnter( other : Collider ) {
+	switch ( SpinDirection ) {
+	case Direction.right:
+		Debug.Log("Direction: right");
+		other.transform.RotateAround (other.transform.position, other.transform.up, 90);
+		break;
+	case Direction.left:
+		Debug.Log("Direction: left");
+		other.transform.RotateAround (other.transform.position, other.transform.down, 90);
+		break;
+	case Direction.back:
+		Debug.Log("Direction: back");
+		worldTrans.Rotate (Vector3.back, 90);
+		break;
+	default:
+		Debug.Log ("No direction choice");
+	}
+	other.transform.position = target.position;
+ }
